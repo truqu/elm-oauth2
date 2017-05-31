139abd7676f863fb52989877ca9c67336936ba2e
KtorZ <matthias.benkort@gmail.com>
2017-05-31 21:53:32 +0200

    implement authorization code flow

---
+++ src/OAuth/AuthorizationCode.elm
@@ -0,0 +1,48 @@
+module OAuth.AuthorizationCode
+    exposing
+        ( authorize
+        , authenticate
+        , parse
+        )
+
+import OAuth exposing (..)
+import Navigation as Navigation
+import Internal as Internal
+import QueryString as QS
+import Http as Http
+
+
+authorize : Authorization -> Cmd msg
+authorize =
+    Internal.authorize
+
+
+authenticate : Authentication -> Http.Request Response
+authenticate =
+    Internal.authenticate
+
+
+parse : Navigation.Location -> Result ParseError Response
+parse { search } =
+    let
+        qs =
+            QS.parse (Debug.log "SEARCH" search)
+
+        gets =
+            flip (QS.one QS.string) qs
+    in
+        case ( gets "code", gets "error" ) of
+            ( Just code, _ ) ->
+                Internal.parseAuthorizationCode
+                    code
+                    (gets "state")
+
+            ( _, Just error ) ->
+                Internal.parseError
+                    error
+                    (gets "error_description")
+                    (gets "error_uri")
+                    (gets "state")
+
+            _ ->
+                Result.Err Empty
