1a5304ff16c965da6366022c68271e9e114e98b7
KtorZ <matthias.benkort@gmail.com>
2018-08-16 10:41:22 +0200

    Update deploy script
    - Add a few comments explaining what each section does
    - Make sure to compile sources AND examples before creating a tag and attempting to publish

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+## Verify nothing is unstaged or untracked
 status=$(git status -s)
 if [ -n "$status" ]; then
   echo "branch isn't clean: commit staged files and / or discard untracked files!"
@@ -7,6 +8,16 @@ if [ -n "$status" ]; then
   exit 1
 fi
 
+## Verify code compiles
+elm make || exit 1
+for d in examples/*; do
+  if [ "$d" != "examples/images" ]; then
+    elm make "$d/Main.elm" || exit 1
+  fi
+done
+rm -f index.html
+
+## Get version number
 version=$(cat elm-package.json | grep '"version"' | sed 's/\([^0-9]*\)\([0-9]\.[0-9]\.[0-9]\)\(.*\)/\2/')
 if [ -z "$version" ]; then
   echo "unable to capture package version"
@@ -15,17 +26,18 @@ else
   echo "VERSION: $version"
 fi
 
+## Create tag and publish
 git tag -a $version -m "release version $version" && git push origin HEAD --tags
 elm package publish || exit 1
-git checkout "gh-pages" || exit 1
 
+## Deploy examples
+git checkout "gh-pages" || exit 1
 for d in examples/*; do
   if [ "$d" != "examples/images" ]; then
     elm make "$d/Main.elm"
     mv index.html $d
   fi
 done
-
 git add . && git commit -m "release version $version"
 git push origin HEAD && git checkout -
 
